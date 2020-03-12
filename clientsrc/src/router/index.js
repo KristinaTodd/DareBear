9f2f0ae2520e2b8b0c3ced714a5c7f1d4825bf2d
jraitter <jraitter@cableone.net>
2020-03-12 16:52:23 -0600

    2 views, styling good, need functions

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreateRoom from '../views/CreateRoom.vue'
+import StartGame from '../views/StartGame.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/createroom',
+    name: 'CreateRoom',
+    component: CreateRoom
+  },
+  {
+    path: '/startgame',
+    name: 'StartGame',
+    component: StartGame
   }
 ]
 
