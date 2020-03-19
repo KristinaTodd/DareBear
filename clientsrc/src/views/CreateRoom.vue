ad8a798150af794b01b129411cc80a722bc6bb35
Kristina Todd <Kristina.Todd22@gmail.com>
2020-03-19 11:19:50 -0600

    local storage

--- clientsrc/src/views/CreateRoom.vue
+++ clientsrc/src/views/CreateRoom.vue
@@ -10,14 +10,8 @@
       <div class="col-1"></div>
       <div class="col-10 text-info">
         <form>
-          <input
-            type="text"
-            class="bg-transparent button-border text-danger input-font"
-            placeholder="name..."
-            v-model="player.playerName"
-            required
-            maxlength="10"
-          />
+          <input type="text" class="bg-transparent button-border text-danger input-font" placeholder="name..."
+            v-model="player.playerName" required maxlength="10" />
         </form>
       </div>
       <div class="col-1"></div>
@@ -32,12 +26,8 @@
       <div class="col-6 text-center">
         <form>
           <div class="form-group">
-            <select
-              v-model="newRoom.roundTotal"
-              class="form-control text-danger bg-transparent button-border"
-              placeholder="Choose"
-              required
-            >
+            <select v-model="newRoom.roundTotal" class="form-control text-danger bg-transparent button-border"
+              placeholder="Choose" required>
               <option class="bg-primary">1</option>
               <option class="bg-primary">2</option>
               <option class="bg-primary">3</option>
@@ -58,99 +48,92 @@
       <div class="col-1"></div>
       <div class="col-10 text-info">
         <form class>
-          <input
-            type="text"
-            class="bg-transparent button-border text-danger input-font"
-            placeholder="room name..."
-            v-model="newRoom.roomName"
-            required
-            maxlength="15"
-          />
+          <input type="text" class="bg-transparent button-border text-danger input-font" placeholder="room name..."
+            v-model="newRoom.roomName" required maxlength="15" />
         </form>
       </div>
       <div class="col-1"></div>
     </div>
     <div class="row text-center py-5">
       <div class="col-1"></div>
-      <div
-        class="col-10 button-border text-danger text-center button-font"
-        @click="createRoom"
-      >Create Room!</div>
+      <div class="col-10 button-border text-danger text-center button-font" @click="createRoom">Create Room!</div>
       <div class="col-1"></div>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from "../components/navbar";
-export default {
-  name: "createroom",
-  components: {
-    Navbar
-  },
-  data() {
-    return {
-      newRoom: {
-        roomCode: Math.floor(Math.random() * 10000),
-        roundTotal: "",
-        roomName: "",
-        players: []
-      },
-      player: {
-        playerName: "",
-        creator: true,
-        imgUrl: `assets/userbear${Math.floor(Math.random() * 12)}.png`
+  import Navbar from "../components/navbar";
+  export default {
+    name: "createroom",
+    components: {
+      Navbar
+    },
+    data() {
+      return {
+        newRoom: {
+          roomCode: Math.floor(Math.random() * 10000),
+          roundTotal: "",
+          roomName: "",
+          players: []
+        },
+        player: {
+          playerName: "",
+          creator: true,
+          imgUrl: `assets/userbear${Math.floor(Math.random() * 12)}.png`
+        }
+      };
+    },
+    methods: {
+      createRoom() {
+        let playerCode = Math.floor(Math.random() * 100000000);
+        this.player.playerCode = playerCode;
+        this.newRoom.playerCode = playerCode;
+        this.newRoom.players.push(this.player);
+        let localPlayerCode = { playerCode: this.player.playerCode }
+        window.localStorage.setItem("localPlayerCode", JSON.stringify(localPlayerCode))
+        this.$store.dispatch("createRoom", this.newRoom);
+        this.$store.dispatch("joinRoom", `room${this.newRoom.roomCode}`);
+        this.$router.push({
+          name: "StartGame",
+          params: { roomCode: this.newRoom.roomCode }
+        });
       }
-    };
-  },
-  methods: {
-    createRoom() {
-      let playerCode = Math.floor(Math.random() * 100000000);
-      this.player.playerCode = playerCode;
-      this.newRoom.playerCode = playerCode;
-      this.newRoom.players.push(this.player);
-      this.$store.dispatch("createRoom", this.newRoom);
-      this.$store.dispatch("joinRoom", `room${this.newRoom.roomCode}`);
-      this.$router.push({
-        name: "StartGame",
-        params: { roomCode: this.newRoom.roomCode }
-      });
     }
-  }
-};
+  };
 </script>
 <style scoped>
-.main-font {
-  font-family: "Gugi", cursive;
-}
+  .main-font {
+    font-family: "Gugi", cursive;
+  }
 
-.info-border {
-  border: 3px;
-  border-style: solid;
-  border-color: #ff2a6d;
-}
+  .info-border {
+    border: 3px;
+    border-style: solid;
+    border-color: #ff2a6d;
+  }
 
-::placeholder {
-  color: #f3739c;
-  opacity: 1;
-}
+  ::placeholder {
+    color: #f3739c;
+    opacity: 1;
+  }
 
-.button-font {
-  font-size: 2rem;
-}
+  .button-font {
+    font-size: 2rem;
+  }
 
-.input-font {
-  font-size: 1.5rem;
-  padding-left: 1rem;
-}
+  .input-font {
+    font-size: 1.5rem;
+    padding-left: 1rem;
+  }
 
-.button-border {
-  border: 3px;
-  border-style: solid;
-  border-color: #05d9e8;
-}
+  .button-border {
+    border: 3px;
+    border-style: solid;
+    border-color: #05d9e8;
+  }
 
-select {
-  width: 10rem;
-}
+  select {
+    width: 10rem;
+  }
 </style>
--- clientsrc/src/views/Dare.vue
+++ clientsrc/src/views/Dare.vue
@@ -25,22 +25,13 @@
     </div>
     <div class="row text-center pt-5">
       <div class="col-1"></div>
-      <div
-        v-show="this.$store.state.me == this.$store.state.room.activePlayer.playerCode"
-        class="col-10 button-border text-danger button-font"
-        @click="modal"
-      >Finished!</div>
+      <div v-show="this.me.localPlayerCode == this.$store.state.room.activePlayer[0].playerCode"
+        class="col-10 button-border text-danger button-font" @click="modal">Finished!</div>
       <div class="col-1"></div>
     </div>
 
-    <div
-      class="modal fade"
-      id="score-modal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalCenterTitle"
-      aria-hidden="true"
-    >
+    <div class="modal fade" id="score-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-body bg-primary text-danger">
@@ -95,107 +86,108 @@
 </template>
 
 <script>
-export default {
-  name: "Dare",
-  mounted() {
-    //this.$store.dispatch("getRoom", this.$store.state.room.roomCode);
-    // console.log("heres our room on this page", this.$store.state.room);
-  },
-  computed: {
-    room() {
-      return this.$store.state.room;
+  export default {
+    name: "Dare",
+    mounted() {
+      //this.$store.dispatch("getRoom", this.$store.state.room.roomCode);
+      // console.log("heres our room on this page", this.$store.state.room);
     },
-    me() {
-      return this.$store.state.me;
-    },
-    activePlayer() {
-      return this.$store.state.room.activePlayer[0];
-    }
-  },
-  data() {
-    return {
-      player: {
-        playerId: this.$store.state.room.activePlayer.id,
-        id: this.$store.state.room.id,
-        roomCode: this.$store.state.room.roomCode,
-        playerCode: this.$store.state.me
+    computed: {
+      room() {
+        return this.$store.state.room;
+      },
+      localPlayerCode() {
+        let me = JSON.parse(window.localStorage.getItem("localPlayerCode"))
+        return me
+      },
+      activePlayer() {
+        return this.$store.state.room.activePlayer[0];
       }
-    };
-  },
-  methods: {
-    async modal() {
-      this.$store.dispatch("modal");
     },
-    async score(num) {
-      this.player.score = num;
-      console.log("Score is", this.player.score);
-      let payload = this.player;
-      await this.$store.dispatch("scorePlayer", this.player);
-      await this.$store.dispatch("updateScored", this.player);
-      if (
-        this.$store.state.room.scored.length ==
+    data() {
+      return {
+        player: {
+          playerId: this.$store.state.room.activePlayer.id,
+          id: this.$store.state.room.id,
+          roomCode: this.$store.state.room.roomCode,
+          playerCode: this.$store.state.me
+        }
+      };
+    },
+    methods: {
+      async modal() {
+        this.$store.dispatch("modal");
+      },
+      async score(num) {
+        this.player.score = num;
+        console.log("Score is", this.player.score);
+        let payload = this.player;
+        await this.$store.dispatch("scorePlayer", this.player);
+        await this.$store.dispatch("updateScored", this.player);
+        if (
+          this.$store.state.room.scored.length ==
           this.$store.state.room.players.length - 1 &&
-        this.$store.state.room.roundCount ==
+          this.$store.state.room.roundCount ==
           this.$store.state.room.roundTotal &&
-        this.$store.state.room.eligiblePlayers.length == 0
-      ) {
-        this.$store.dispatch("endGame", payload);
-      } else if (
-        this.$store.state.room.scored.length ==
+          this.$store.state.room.eligiblePlayers.length == 0
+        ) {
+          this.$store.dispatch("endGame", payload);
+        } else if (
+          this.$store.state.room.scored.length ==
           this.$store.state.room.players.length - 1 &&
-        this.$store.state.room.eligiblePlayers.length == 0
-      ) {
-        await this.$store.dispatch("updateScored", payload);
-        this.$store.dispatch("endRound", payload);
-      } else if (
-        this.$store.state.room.scored.length ==
-        this.$store.state.room.players.length - 1
-      ) {
-        await this.$store.dispatch("updateScored", payload);
-        this.$store.dispatch("endTurn", payload);
-        this.$store.dispatch("editActive", payload);
-      } else {
-        this.$store.dispatch("waitingView");
+          this.$store.state.room.eligiblePlayers.length == 0
+        ) {
+          await this.$store.dispatch("updateScored", payload);
+          this.$store.dispatch("endRound", payload);
+        } else if (
+          this.$store.state.room.scored.length ==
+          this.$store.state.room.players.length - 1
+        ) {
+          await this.$store.dispatch("updateScored", payload);
+          this.$store.dispatch("endTurn", payload);
+          this.$store.dispatch("editActive", payload);
+        } else {
+          this.$store.dispatch("waitingView");
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style>
-.info-border {
-  border: 5px;
-  border-style: solid;
-  border-color: #ff2a6d;
-}
+  .info-border {
+    border: 5px;
+    border-style: solid;
+    border-color: #ff2a6d;
+  }
 
-.button-border {
-  border: 3px;
-  border-style: solid;
-  border-color: #05d9e8;
-}
+  .button-border {
+    border: 3px;
+    border-style: solid;
+    border-color: #05d9e8;
+  }
 
-.button-font {
-  font-size: 2rem;
-}
+  .button-font {
+    font-size: 2rem;
+  }
 
-.main-font {
-  font-family: "Gugi", cursive;
-}
+  .main-font {
+    font-family: "Gugi", cursive;
+  }
 
-.img-width {
-  width: 14rem;
-}
+  .img-width {
+    width: 14rem;
+  }
 
-.small-text {
-  font-size: 1.2rem;
-}
+  .small-text {
+    font-size: 1.2rem;
+  }
 
-.title-font {
-  font-size: 2rem;
-}
+  .title-font {
+    font-size: 2rem;
+  }
 
-.score-font {
-  font-size: 4rem;
-}
+  .score-font {
+    font-size: 4rem;
+  }
 </style>
