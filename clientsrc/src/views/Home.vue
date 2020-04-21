de098c88e8c0e4c328447fbc2f2f5e0caaedd67b
Kristina Todd <Kristina.Todd22@gmail.com>
2020-04-21 13:57:27 -0600

    removed log in button from home page

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -32,52 +32,45 @@
       </div>
       <div class="col-1"></div>
     </div>
-    <div class="row m-2 text-danger button-font mt-4">
-      <div class="col-1"></div>
-      <div class="col-10 d-flex justify-content-center button-border">
-        <div>Log In</div>
-      </div>
-      <div class="col-1"></div>
-    </div>
   </div>
 </template>
 
 <script>
-import Navbar from "../components/navbar";
-export default {
-  name: "Home",
-  components: {
-    Navbar
-  },
-  methods: {
-    joinRoom() {
-      this.$router.push({ name: "JoinRoom" });
+  import Navbar from "../components/navbar";
+  export default {
+    name: "Home",
+    components: {
+      Navbar
     },
-    createRoom() {
-      this.$router.push({ name: "CreateRoom" });
+    methods: {
+      joinRoom() {
+        this.$router.push({ name: "JoinRoom" });
+      },
+      createRoom() {
+        this.$router.push({ name: "CreateRoom" });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
-.info-border {
-  border: 5px;
-  border-style: solid;
-  border-color: #ff2a6d;
-}
+  .info-border {
+    border: 5px;
+    border-style: solid;
+    border-color: #ff2a6d;
+  }
 
-.button-border {
-  border: 3px;
-  border-style: solid;
-  border-color: #05d9e8;
-}
+  .button-border {
+    border: 3px;
+    border-style: solid;
+    border-color: #05d9e8;
+  }
 
-.button-font {
-  font-size: 2rem;
-}
+  .button-font {
+    font-size: 2rem;
+  }
 
-.main-font {
-  font-family: "Gugi", cursive;
-}
+  .main-font {
+    font-family: "Gugi", cursive;
+  }
 </style>
